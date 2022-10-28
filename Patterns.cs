8f7f196ec23ae839a4c6e7c1f8cef84da17aeaf8
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-28 11:57:20 +0530

    Pattern for First name

---
+++ Patterns.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegxDemo
+{
+    public class Patterns
+    {
+        public  String REGX_FNAME = "^[A-Z]{1}[A-Z a-z]{2,3}$";
+        public bool boolvalidate(string Fname)
+        {
+            return Regex.IsMatch(Fname, REGX_FNAME);
+        }
+    }
+}
