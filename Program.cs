71527ff9868dbee8034145c68346428d2e3e39b0
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-28 11:58:51 +0530

    UseCase 1

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace RegxDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the First Name: ");
+            string Fname = Console.ReadLine();
+            Patterns patterns = new Patterns();
+            bool val= patterns.boolvalidate(Fname);
+            if(val)
+            {
+                Console.WriteLine("Valid Fname");
+
+             }
+            else
+            {
+                Console.WriteLine("Invalid Fname");
+            }
+        }
+    }
+}
